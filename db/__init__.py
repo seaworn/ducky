dcb45b40fc28a113061822764891201e706aa64d
eric <eric@sasalog.com>
2022-09-06 13:04:30 +0300

    fix: automatically begin session and commit/rollback

--- db/__init__.py
+++ db/__init__.py
@@ -1,13 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker
-
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = 'sqlite+aiosqlite:///db.sqlite'
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-Base = declarative_base()
+AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base(engine)
 
 
 async def get_session() -> AsyncSession:
-    async with async_session() as session:
+    async with AsyncSessionLocal.begin() as session:
         yield session
